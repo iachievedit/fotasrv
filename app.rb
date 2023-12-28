c4f867d05647acb3a0a59347fb824508974e307a
iachievedit <joe@iachieved.it>
2023-12-28 12:36:09 -0600

    Initial

--- app.rb
+++ app.rb
@@ -2,20 +2,32 @@ require 'sinatra'
 require 'json'
 
 class FotaServer < Sinatra::Base
-get '/manifest' do
-  file_path = 'path_to_file.json'
-
-  if File.exist?(file_path)
-    content = File.read(file_path)
-    content_type :json
-    content
-  else
-    status 404
-    { error: 'File not found' }.to_json
+  get '/system' do
+    file_path = 'REVISION'
+    if File.exist?(file_path)
+      content = File.read(file_path)
+      content_type :json
+      { revision: content }.to_json
+    else
+      status 404
+      { error: 'Revision information unavailable' }.to_json
+    end
   end
-end
 
-get '/firmware' do
-  'Firmware route - under construction'
-end
+  get '/manifest' do
+    file_path = '/apps/fotasrv/shared/manifest.json'
+
+    if File.exist?(file_path)
+      content = File.read(file_path)
+      content_type :json
+      content
+    else
+      status 404
+      { error: 'Firmware manifest file not found' }.to_json
+    end
+  end
+
+  get '/firmware' do
+    'Firmware route - under construction'
+  end
 end
