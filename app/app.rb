4440d1f7d66bfaa5300d2971372b1d32a5859144
iachievedit <joe@iachieved.it>
2023-12-28 11:10:36 -0600

    Initial version

---
+++ app/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'json'
+
+get '/manifest' do
+  file_path = 'path_to_file.json'
+
+  if File.exist?(file_path)
+    content = File.read(file_path)
+    content_type :json
+    content
+  else
+    status 404
+    { error: 'File not found' }.to_json
+  end
+end
+
+get '/firmware' do
+  'Firmware route - under construction'
+end
+
